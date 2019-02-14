28c1f7ceb330385b80596654a5e8a771d5f54c69
PythonDarkening <humbertoisraelrodriguez@gmail.com>
2019-02-13 20:36:57 -0600

    update files

--- src/array_remove_element_right_left.java
+++ src/array_remove_element_right_left.java
@@ -7,23 +7,31 @@ public class array_remove_element_right_left {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        //int[] intArr = new int[7]; // declare an array with 7 empty spaces
         int[] intArr = {1, 2, 2, 12, 3, 3, 8};
 
         System.out.println("\n== Right to left removal ==\n");
 
-        System.out.print("Original array is: " + Arrays.toString(intArr) + "\n\n");
+        if(intArr[0] != 0){
+            System.out.print("Original array is: " + Arrays.toString(intArr) + "\n\n");
 
-        System.out.print("Enter the element to be deleted: ");
-        int elem = in.nextInt();
+            System.out.print("Enter the element to be deleted: ");
+            int elem = in.nextInt();
 
-        for(int i = intArr.length -1; i >= 0; i--){
-            // search for the first match and exit the loop
-            if(intArr[i] == elem){
-                intArr[i] = 0;
-                break;
+            for(int i = intArr.length -1; i >= 0; i--){
+                // search for the first match and exit the loop
+                if(intArr[i] == elem){
+                    intArr[i] = 0;
+                    break;
+                }
             }
+
+            System.out.print("\nModified array is: " + Arrays.toString(intArr) + "\n\n");
+        }
+        else{
+            System.out.println("The array is empty");
         }
 
-        System.out.print("\nModified array is: " + Arrays.toString(intArr) + "\n\n");
+
     }
 }
